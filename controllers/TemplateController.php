ea0b94757d7f6712681b2d1ec41e84322f5d7d8a
Yuri Doronin <dye491@gmail.com>
2018-06-13 18:34:15 +0200

    set default route

--- controllers/TemplateController.php
+++ controllers/TemplateController.php
@@ -6,6 +6,7 @@ use app\models\Template;
 use yii\helpers\Json;
 use yii\data\ActiveDataProvider;
 use yii\web\Controller;
+use yii\web\UploadedFile;
 
 class TemplateController extends Controller
 {
@@ -22,12 +23,12 @@ class TemplateController extends Controller
 
     public function actionUpdate($id)
     {
-        $model = Template::findOne(['id' => $id]);
+        $model = self::findModel($id);
         $vars = $model->getVariablesWithValues();
 
         if (\Yii::$app->request->isPost) {
             $postParams = \Yii::$app->request->post();
-            $varKeys = array_map(function($item) {
+            $varKeys = array_map(function ($item) {
                 return isset($item['name']) ? $item['name'] : null;
             }, $model->getVariables()->asArray()->all());
             foreach ($postParams as $key => $postParam) {
@@ -47,4 +48,37 @@ class TemplateController extends Controller
         ]);
     }
 
+    public function actionEdit($id)
+    {
+        $model = self::findModel($id);
+        $dataProvider = new ActiveDataProvider([
+            'query' => $model->getVariables(),
+        ]);
+
+        if ($model->load(\Yii::$app->request->post()) /*&& $model->validate()*/) {
+            $uploadedFile = UploadedFile::getInstance($model, 'templateFile');
+            $model->templateFile = $uploadedFile;
+            if ($model->validate()) {
+                if ($uploadedFile->error == UPLOAD_ERR_OK &&
+                    $uploadedFile->saveAs(\Yii::getAlias(Template::TEMPLATE_DIR) .
+                        DIRECTORY_SEPARATOR . $uploadedFile->baseName . '.' . $uploadedFile->extension)
+                ) {
+                    $model->file_name = $uploadedFile->baseName . '.' . $uploadedFile->extension;
+                }
+                if ($model->save(false)) {
+                    return $this->redirect('/template');
+                }
+            }
+        }
+
+        return $this->render('edit', [
+            'model'        => $model,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    private static function findModel($id)
+    {
+        return Template::findOne(['id' => $id]);
+    }
 }
