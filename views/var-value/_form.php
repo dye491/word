7caf1bc731d0818d163d24c83119e42900932b1c
Yuri Doronin <dye491@gmail.com>
2018-07-24 10:10:21 +0200

    clean-up view code

--- views/var-value/_form.php
+++ views/var-value/_form.php
@@ -2,7 +2,6 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use yii\jui\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\VarValue */
@@ -13,21 +12,7 @@ use yii\jui\DatePicker;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <!--    --><? //= $form->field($model, 'company_id')->textInput() ?>
-
-    <!--    --><? //= $form->field($model, 'var_id')->textInput() ?>
-
     <?= $form->field($model, 'value')->textInput(['maxlength' => true]) ?>
-
-    <?/*= $form->field($model, 'start_date')->widget(DatePicker::class, [
-        'options' => ['class' => 'form-control'],
-        'dateFormat' => 'php:d.m.Y',
-    ]) */?><!--
-
-    --><?/*= $form->field($model, 'end_date')->widget(DatePicker::class, [
-        'options' => ['class' => 'form-control'],
-        'dateFormat' => 'php:d.m.Y',
-    ]) */?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
